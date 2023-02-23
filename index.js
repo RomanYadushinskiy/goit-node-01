d92745a9b0c0bdfa768c4e49bc88605a55e32ec7
rdush <rdush824@gmail.com>
2023-02-23 21:33:42 +0200

    hw done

---
+++ index.js
@@ -0,0 +1,77 @@
+
+// const argv = require("yargs").argv;
+
+// const { listContacts, getContactById, removeContact, addContact } = require("./contacts");
+
+// async function invokeAction({ action, id, name, email, phone }) {
+//   switch (action) {
+//     case "list":
+//       const contacts = await listContacts();
+//       console.table(contacts)
+//       break;
+
+//     case "get":
+//       const contact = await getContactById(id);
+//       console.log("in index.js:", contact);
+//       break;
+
+//     case "add":
+//       await addContact(name, email, phone)
+//       break;
+
+//     case "remove":
+//       await removeContact(id)
+//       break;
+
+//     default:
+//       console.warn("\x1B[31m Unknown action type!");
+//   }
+// }
+// invokeAction(argv)
+
+const { program } = require('commander');
+
+const { listContacts, getContactById, removeContact, addContact } = require('./contacts');
+
+program
+	.option('-a, --action <type>', 'choose action')
+	.option('-i, --id <type>', 'user id')
+	.option('-n, --name <type>', 'user name')
+	.option('-e, --email <type>', 'user email')
+	.option('-p, --phone <type>', 'user phone')
+
+program.parse(process.argv)
+
+const argv = program.opts()
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'list':
+      const contacts = await listContacts()
+      console.table(contacts);
+      break;
+
+    case 'get':
+      const contact = await getContactById(id)
+      if (!contact) {
+        throw new Error(`Contact with id=${id} not found`);
+      }
+      console.table(contact);
+      break;
+
+    case 'add':
+      const newContact = await addContact({name, email, phone})
+      console.table(newContact);
+      break;
+
+    case 'remove':
+      const removeCont = await removeContact(id)
+      console.table(removeCont);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  };
+};
+
+invokeAction(argv)
